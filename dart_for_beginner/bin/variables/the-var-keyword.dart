9b364bd62c988d246b7fb6b617053bb2ec186f47
songhee <ereteyeu@naver.com>
2023-03-27 15:11:12 +0900

    니꼬쌤의 다트기본 교육 > 프로젝트 세팅, 변수

---
+++ dart_for_beginner/bin/variables/the-var-keyword.dart
@@ -0,0 +1,13 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main() {
+  var name = '송희';
+  name = 'songhee';
+  print(name);
+  // 관습적으로 함수나 메소드 내부에 지역변수를 선얼할 때는 var 사용
+
+  String name2 = '송희';
+  name2 = 'songhee2';
+  print(name2);
+  // class에서 변수나 property를 선언할 때에는 타입을 지정해준다.
+}
